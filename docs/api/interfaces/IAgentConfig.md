1124319d0679cb9d6345b6a13cfcc921e728073d
ZillyOS <davidjbailey.personal@gmail.com>
2025-05-22 15:32:17 -0400

    Complete Mission 01 setup: Project structure, configuration, and development environment

--- docs/api/interfaces/IAgentConfig.md
+++
@@ -1,7 +0,0 @@
-[@elizaos/core v0.25.9](../index.md) / IAgentConfig
-
-# Interface: IAgentConfig
-
-## Indexable
-
- \[`key`: `string`\]: `string`
--- packages/core-plugin-v1/src/database.ts
+++
@@ -1,5 +0,0 @@
-import {
-  type DatabaseAdapter as coreDatabaseAdapter,
-} from '@elizaos/core';
-
-export type DatabaseAdapter = coreDatabaseAdapter;
